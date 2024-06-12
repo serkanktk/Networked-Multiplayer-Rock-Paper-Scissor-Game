560d5bc649f0a3736ae13457a2d17a7df09858e6
serkanktk <128151657+serkanktk@users.noreply.github.com>
2024-06-12 14:47:44 +0300

    Server and Client Programs added

---
+++ Client/FinalClientTemiz/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace FinalClientTemiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IpLabel = new System.Windows.Forms.Label();
+            this.IpTextBox = new System.Windows.Forms.TextBox();
+            this.PortLabel = new System.Windows.Forms.Label();
+            this.PortTextBox = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.RockButton = new System.Windows.Forms.Button();
+            this.PaperButton = new System.Windows.Forms.Button();
+            this.ScissorsButton = new System.Windows.Forms.Button();
+            this.LeaveTheGameButton = new System.Windows.Forms.Button();
+            this.GAMElabel = new System.Windows.Forms.Label();
+            this.GameRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.UPDATEDlabel = new System.Windows.Forms.Label();
+            this.UpdatesRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // IpLabel
+            // 
+            this.IpLabel.AutoSize = true;
+            this.IpLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.IpLabel.Location = new System.Drawing.Point(49, 46);
+            this.IpLabel.Name = "IpLabel";
+            this.IpLabel.Size = new System.Drawing.Size(36, 25);
+            this.IpLabel.TabIndex = 1;
+            this.IpLabel.Text = "IP:";
+            // 
+            // IpTextBox
+            // 
+            this.IpTextBox.Location = new System.Drawing.Point(100, 51);
+            this.IpTextBox.Name = "IpTextBox";
+            this.IpTextBox.Size = new System.Drawing.Size(137, 20);
+            this.IpTextBox.TabIndex = 3;
+            // 
+            // PortLabel
+            // 
+            this.PortLabel.AutoSize = true;
+            this.PortLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.PortLabel.Location = new System.Drawing.Point(260, 45);
+            this.PortLabel.Name = "PortLabel";
+            this.PortLabel.Size = new System.Drawing.Size(53, 25);
+            this.PortLabel.TabIndex = 4;
+            this.PortLabel.Text = "Port:";
+            // 
+            // PortTextBox
+            // 
+            this.PortTextBox.Location = new System.Drawing.Point(319, 46);
+            this.PortTextBox.Name = "PortTextBox";
+            this.PortTextBox.Size = new System.Drawing.Size(137, 20);
+            this.PortTextBox.TabIndex = 5;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(487, 46);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(137, 24);
+            this.ConnectButton.TabIndex = 6;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.NameLabel.Location = new System.Drawing.Point(49, 120);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(224, 25);
+            this.NameLabel.TabIndex = 7;
+            this.NameLabel.Text = "Please enter your name:";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Enabled = false;
+            this.NameTextBox.Location = new System.Drawing.Point(297, 125);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(137, 20);
+            this.NameTextBox.TabIndex = 8;
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Enabled = false;
+            this.EnterButton.Location = new System.Drawing.Point(464, 125);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(137, 24);
+            this.EnterButton.TabIndex = 9;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // RockButton
+            // 
+            this.RockButton.Enabled = false;
+            this.RockButton.Location = new System.Drawing.Point(54, 190);
+            this.RockButton.Name = "RockButton";
+            this.RockButton.Size = new System.Drawing.Size(137, 24);
+            this.RockButton.TabIndex = 13;
+            this.RockButton.Text = "Rock";
+            this.RockButton.UseVisualStyleBackColor = true;
+            this.RockButton.Click += new System.EventHandler(this.RockButton_Click);
+            // 
+            // PaperButton
+            // 
+            this.PaperButton.Enabled = false;
+            this.PaperButton.Location = new System.Drawing.Point(229, 190);
+            this.PaperButton.Name = "PaperButton";
+            this.PaperButton.Size = new System.Drawing.Size(137, 24);
+            this.PaperButton.TabIndex = 14;
+            this.PaperButton.Text = "Paper";
+            this.PaperButton.UseVisualStyleBackColor = true;
+            this.PaperButton.Click += new System.EventHandler(this.PaperButton_Click);
+            // 
+            // ScissorsButton
+            // 
+            this.ScissorsButton.Enabled = false;
+            this.ScissorsButton.Location = new System.Drawing.Point(388, 190);
+            this.ScissorsButton.Name = "ScissorsButton";
+            this.ScissorsButton.Size = new System.Drawing.Size(137, 24);
+            this.ScissorsButton.TabIndex = 15;
+            this.ScissorsButton.Text = "Scissors";
+            this.ScissorsButton.UseVisualStyleBackColor = true;
+            this.ScissorsButton.Click += new System.EventHandler(this.ScissorsButton_Click);
+            // 
+            // LeaveTheGameButton
+            // 
+            this.LeaveTheGameButton.Enabled = false;
+            this.LeaveTheGameButton.Location = new System.Drawing.Point(544, 190);
+            this.LeaveTheGameButton.Name = "LeaveTheGameButton";
+            this.LeaveTheGameButton.Size = new System.Drawing.Size(137, 24);
+            this.LeaveTheGameButton.TabIndex = 16;
+            this.LeaveTheGameButton.Text = "Leave The Game";
+            this.LeaveTheGameButton.UseVisualStyleBackColor = true;
+            this.LeaveTheGameButton.Click += new System.EventHandler(this.LeaveTheGameButton_Click);
+            // 
+            // GAMElabel
+            // 
+            this.GAMElabel.AutoSize = true;
+            this.GAMElabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.GAMElabel.Location = new System.Drawing.Point(95, 241);
+            this.GAMElabel.Name = "GAMElabel";
+            this.GAMElabel.Size = new System.Drawing.Size(71, 25);
+            this.GAMElabel.TabIndex = 17;
+            this.GAMElabel.Text = "GAME";
+            // 
+            // GameRichTextBox
+            // 
+            this.GameRichTextBox.Location = new System.Drawing.Point(32, 279);
+            this.GameRichTextBox.Name = "GameRichTextBox";
+            this.GameRichTextBox.Size = new System.Drawing.Size(305, 137);
+            this.GameRichTextBox.TabIndex = 18;
+            this.GameRichTextBox.Text = "";
+            // 
+            // UPDATEDlabel
+            // 
+            this.UPDATEDlabel.AutoSize = true;
+            this.UPDATEDlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.UPDATEDlabel.Location = new System.Drawing.Point(471, 241);
+            this.UPDATEDlabel.Name = "UPDATEDlabel";
+            this.UPDATEDlabel.Size = new System.Drawing.Size(107, 25);
+            this.UPDATEDlabel.TabIndex = 19;
+            this.UPDATEDlabel.Text = "UPDATES";
+            // 
+            // UpdatesRichTextBox
+            // 
+            this.UpdatesRichTextBox.Location = new System.Drawing.Point(362, 279);
+            this.UpdatesRichTextBox.Name = "UpdatesRichTextBox";
+            this.UpdatesRichTextBox.Size = new System.Drawing.Size(319, 137);
+            this.UpdatesRichTextBox.TabIndex = 20;
+            this.UpdatesRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UpdatesRichTextBox);
+            this.Controls.Add(this.UPDATEDlabel);
+            this.Controls.Add(this.GameRichTextBox);
+            this.Controls.Add(this.GAMElabel);
+            this.Controls.Add(this.LeaveTheGameButton);
+            this.Controls.Add(this.ScissorsButton);
+            this.Controls.Add(this.PaperButton);
+            this.Controls.Add(this.RockButton);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.PortTextBox);
+            this.Controls.Add(this.PortLabel);
+            this.Controls.Add(this.IpTextBox);
+            this.Controls.Add(this.IpLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label IpLabel;
+        private System.Windows.Forms.TextBox IpTextBox;
+        private System.Windows.Forms.Label PortLabel;
+        private System.Windows.Forms.TextBox PortTextBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.Button RockButton;
+        private System.Windows.Forms.Button PaperButton;
+        private System.Windows.Forms.Button ScissorsButton;
+        private System.Windows.Forms.Button LeaveTheGameButton;
+        private System.Windows.Forms.Label GAMElabel;
+        private System.Windows.Forms.RichTextBox GameRichTextBox;
+        private System.Windows.Forms.Label UPDATEDlabel;
+        private System.Windows.Forms.RichTextBox UpdatesRichTextBox;
+    }
+}
+
---
+++ Client/FinalClientTemiz/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalClientTemiz
+{
+    public partial class Form1 : Form
+    {
+
+        // Variables to track connection status and state
+        bool terminating = false;
+        bool connected = false;
+        bool isNamed = false;
+        string currentUserName = null;
+        Socket clientSocket;
+
+        // Constructor called when the form is opened
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+
+            InitializeComponent();
+        }
+
+        // Event handler called when the form is closing
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connected = false;
+
+            terminating = true;
+
+            Environment.Exit(0);
+        }
+
+        // Event handler called when the Connect button is clicked
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            string IP = IpTextBox.Text;
+
+            int portNum;
+
+            // Try to parse the port number from the text box
+            if (Int32.TryParse(PortTextBox.Text, out portNum))
+            {
+                try
+                {
+                    clientSocket.Connect(IP, portNum);
+
+                    // Disable the IP and Port text boxes and the Connect button
+                    IpTextBox.Enabled = false;
+                    PortTextBox.Enabled = false;
+                    ConnectButton.Enabled = false;
+
+                    NameTextBox.Enabled = true;
+                    EnterButton.Enabled = true;
+
+                    connected = true;
+
+                    // Notify the user that the connection was successful
+                    UpdatesRichTextBox.AppendText("Connected to the server!\n");
+
+                    Thread receiveThread = new Thread(ReceiveClient);
+                    receiveThread.Start();
+                }
+                catch
+                {
+                    // Notify the user that the connection failed
+                    UpdatesRichTextBox.AppendText("Could not connect to the server!\n");
+                }
+            }
+            else
+            {
+                // Notify the user that the port number is invalid
+                UpdatesRichTextBox.AppendText("Check the port\n");
+            }
+        }
+
+        // Event handler called when the Enter button is clicked
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            string message = NameTextBox.Text;
+
+            if (message != "")
+            {
+                if (message.Length <= 64)
+                {
+                    // Encode the name with a prefix "NN" and send it to the server
+                    Byte[] buffer = Encoding.Default.GetBytes("NN" + message);
+                    clientSocket.Send(buffer);
+                }
+                else
+                {
+                    // Notify the user that the name is too long
+                    UpdatesRichTextBox.AppendText("Please enter a shorter name!\n");
+                }
+            }
+            else
+            {
+                // Notify the user that the name cannot be empty
+                UpdatesRichTextBox.AppendText("The name cannot be empty!\n");
+            }
+        }
+
+
+        // instance'ın message içerisindeki 2. posizyonunu döndüren fonksiyon
+        private static int FindSecondOccurrence(string message, string instance)
+        {
+            int firstIndex = message.IndexOf(instance);
+
+            if (firstIndex == -1)
+            {
+                return -1;
+            }
+
+            int secondIndex = message.IndexOf(instance, firstIndex + instance.Length);
+
+            return secondIndex;
+        }
+
+
+
+        private void ReceiveClient()
+        {
+            while (connected)
+            {
+                try
+                {
+                    // Buffer to store incoming data
+                    Byte[] buffer = new Byte[128];
+                    clientSocket.Receive(buffer);
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+
+                    // Append the incoming message to the UpdatesRichTextBox
+                    UpdatesRichTextBox.AppendText("_____________________\n");
+                    UpdatesRichTextBox.AppendText("The incoming message from server: " + incomingMessage + "\n");
+
+                    // Check if the name is confirmed
+                    if (incomingMessage.StartsWith("NN"))
+                    {
+                        // If the name is for the current user
+                        if (incomingMessage.Substring(FindSecondOccurrence(incomingMessage, "NN") + 2) == NameTextBox.Text)
+                        {
+                            currentUserName = incomingMessage.Substring(FindSecondOccurrence(incomingMessage, "NN") + 2);
+                            UpdatesRichTextBox.AppendText("Welcome to server " + currentUserName + " (Win count: " + incomingMessage.Substring(2, FindSecondOccurrence(incomingMessage, "NN") - 2) + ")\n");
+                            NameTextBox.Enabled = false;
+                            EnterButton.Enabled = false;
+                        }
+                        // If the name is not for the current user
+                        else
+                        {
+                            UpdatesRichTextBox.AppendText(incomingMessage.Substring(FindSecondOccurrence(incomingMessage, "NN") + 2) + " (Win count: " + incomingMessage.Substring(2, FindSecondOccurrence(incomingMessage, "NN") - 2) + ") joined the server.\n");
+                        }
+                    }
+
+                    // If the name is already taken
+                    if (incomingMessage == "NameTaken")
+                    {
+                        UpdatesRichTextBox.AppendText("Please enter another name. The name is taken.\n");
+                    }
+
+                    // If a new player joins the game
+                    if (incomingMessage.StartsWith("JG"))
+                    {
+                        // If the current user joins the game
+                        if (incomingMessage.Substring(2) == currentUserName)
+                        {
+                            LeaveTheGameButton.Enabled = true;
+                        }
+                        GameRichTextBox.AppendText(incomingMessage.Substring(2) + " joined the game.\n");
+                    }
+
+                    // If the game is closed
+                    if (incomingMessage.StartsWith("CloseOff"))
+                    {
+                        LeaveTheGameButton.Enabled = false;
+                        RockButton.Enabled = false;
+                        PaperButton.Enabled = false;
+                        ScissorsButton.Enabled = false;
+                    }
+
+                    // If the player is put in the queue
+                    if (incomingMessage.StartsWith("QU"))
+                    {
+                        UpdatesRichTextBox.AppendText("You are in queue.\n");
+                    }
+
+                    // If countdown messages are received
+                    if (incomingMessage.StartsWith("Count"))
+                    {
+                        GameRichTextBox.AppendText(incomingMessage.Substring(5) + "\n");
+                        if (incomingMessage.Substring(5) == "Start")
+                        {
+                            LeaveTheGameButton.Enabled = true;
+                            // ScissorsButton.Enabled = true;
+                            // PaperButton.Enabled = true;
+                            // RockButton.Enabled = true;
+                        }
+                    }
+
+                    // If a player leaves the countdown
+                    if (incomingMessage.StartsWith("LC"))
+                    {
+                        
+                        if (incomingMessage.Substring(2) == currentUserName)
+                        {
+                            LeaveTheGameButton.Enabled = false;
+                            
+                        }
+                        else if(incomingMessage.Substring(3) == currentUserName)
+                        {
+                            LeaveTheGameButton.Enabled = false;
+                            RockButton.Enabled = false;
+                            PaperButton.Enabled = false;
+                            ScissorsButton.Enabled = false;
+                            
+                        }
+
+                        if(incomingMessage.Substring(0,3) == "LCC")
+                        {
+                            GameRichTextBox.AppendText(incomingMessage.Substring(3) + " left the game.\n");
+                        }
+                        else
+                        {
+                            GameRichTextBox.AppendText(incomingMessage.Substring(2) + " left the countdown.\n");
+                        }
+
+
+                    }
+
+
+                    if(incomingMessage == "lock")
+                    {
+                        LeaveTheGameButton.Enabled = false;
+                        ScissorsButton.Enabled = false;
+                        PaperButton.Enabled = false;
+                        RockButton.Enabled = false;
+                    }
+
+
+
+
+                    // If a RESTART message is received
+                    if (incomingMessage == "RESTART")
+                    {
+                        Byte[] buf = Encoding.Default.GetBytes("RESTART");
+                        clientSocket.Send(buf);
+                    }
+
+                    // If game countdown messages are received
+                    if (incomingMessage.StartsWith("Game"))
+                    {
+                        if (incomingMessage.Substring(4) == "C10")
+                        {
+                            GameRichTextBox.AppendText(incomingMessage.Substring(5) + "\n");
+                            LeaveTheGameButton.Enabled = true;
+                            ScissorsButton.Enabled = true;
+                            PaperButton.Enabled = true;
+                            RockButton.Enabled = true;
+                        }
+                        else
+                        {
+                            GameRichTextBox.AppendText(incomingMessage.Substring(4) + "\n");
+                        }
+                    }
+
+                    // If a player is eliminated
+                    if (incomingMessage.StartsWith("Eliminated"))
+                    {
+                        if (incomingMessage.Substring(10) == "NoOne")
+                        {
+                            GameRichTextBox.AppendText("No one eliminated in this round.\n");
+                        }
+                        else
+                        {
+                            GameRichTextBox.AppendText(incomingMessage.Substring(10) + " is eliminated.\n");
+                        }
+                    }
+
+                    // If activation message is received
+                    if (incomingMessage == "activate")
+                    {
+                        LeaveTheGameButton.Enabled = true;
+                        RockButton.Enabled = true;
+                        PaperButton.Enabled = true;
+                        ScissorsButton.Enabled = true;
+                    }
+
+                    // If a winner is announced
+                    if (incomingMessage.StartsWith("Winner"))
+                    {
+                        GameRichTextBox.AppendText(incomingMessage.Substring(6) + " wins the game.\n");
+                        LeaveTheGameButton.Enabled = false;
+                        RockButton.Enabled = false;
+                        PaperButton.Enabled = false;
+                        ScissorsButton.Enabled = false;
+                    }
+
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        UpdatesRichTextBox.AppendText("The server has disconnected\n");
+
+                        IpTextBox.Enabled = true;
+                        PortTextBox.Enabled = true;
+                        ConnectButton.Enabled = true;
+
+                        NameTextBox.Enabled = false;
+                        EnterButton.Enabled = false;
+                        RockButton.Enabled = false;
+                        PaperButton.Enabled = false;
+                        ScissorsButton.Enabled = false;
+                        LeaveTheGameButton.Enabled = false;
+                        isNamed = false;
+                    }
+
+                    clientSocket.Close();
+                    connected = false;
+                }
+            }
+        }
+        private void LeaveTheGameButton_Click(object sender, EventArgs e)
+        {
+            // Send a message to the server indicating that the current user is leaving the game
+            Byte[] buffer = Encoding.Default.GetBytes("LG" + currentUserName);
+            clientSocket.Send(buffer);
+        }
+
+        private void RockButton_Click(object sender, EventArgs e)
+        {
+            // Append the chosen move to the GameRichTextBox
+            GameRichTextBox.AppendText("You have chosen Rock\n");
+            // Disable all the game buttons to prevent further input
+            RockButton.Enabled = false;
+            PaperButton.Enabled = false;
+            ScissorsButton.Enabled = false;
+            LeaveTheGameButton.Enabled = false;
+            // Send the chosen move to the server
+            Byte[] buffer = Encoding.Default.GetBytes("Rock" + currentUserName);
+            clientSocket.Send(buffer);
+        }
+
+        private void PaperButton_Click(object sender, EventArgs e)
+        {
+            // Append the chosen move to the GameRichTextBox
+            GameRichTextBox.AppendText("You have chosen Paper\n");
+            // Disable all the game buttons to prevent further input
+            RockButton.Enabled = false;
+            PaperButton.Enabled = false;
+            ScissorsButton.Enabled = false;
+            LeaveTheGameButton.Enabled = false;
+            // Send the chosen move to the server
+            Byte[] buffer = Encoding.Default.GetBytes("Paper" + currentUserName);
+            clientSocket.Send(buffer);
+        }
+
+        private void ScissorsButton_Click(object sender, EventArgs e)
+        {
+            // Append the chosen move to the GameRichTextBox
+            GameRichTextBox.AppendText("You have chosen Scissors.\n");
+            // Disable all the game buttons to prevent further input
+            RockButton.Enabled = false;
+            PaperButton.Enabled = false;
+            ScissorsButton.Enabled = false;
+            LeaveTheGameButton.Enabled = false;
+            // Send the chosen move to the server
+            Byte[] buffer = Encoding.Default.GetBytes("Scissors" + currentUserName);
+            clientSocket.Send(buffer);
+        }
+
+    }
+}
